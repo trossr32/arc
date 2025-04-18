76970f4cc6b522d286a638a237efc93608bc46df
Rob Green <rob@robgreen.me>
2025-04-18 15:17:46 +0100

    move data

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -9,7 +9,7 @@ const Products: React.FC = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch('/src/data/products.json');
+        const response = await fetch('/data/products.json');
         const data = await response.json();
         setProducts(data.products);
         setLoading(false);
